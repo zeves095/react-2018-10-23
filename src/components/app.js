8c9c7730ff28879dea3bd763cd770834da169599
Kirill Lavrov <zeves095@gmail.com>
2018-10-29 13:47:57 +0300

    Add react-day-picker component to filter articles

--- src/components/app.js
+++ src/components/app.js
@@ -3,12 +3,26 @@ import ArticleList from './article-list'
 import articles from '../fixtures'
 import UserForm from './user-form'
 import Select from 'react-select'
+import DayPicker, { DateUtils } from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
+
+const birthdayStyle = `.DayPicker-Day--highlighted {
+  background-color: orange;
+  color: white;
+}`
 
 export default class App extends Component {
   state = {
-    selectedOption: null
+    selectedOption: null,
+    selectedDay: undefined,
+    days: {
+      from: undefined,
+      to: undefined
+    }
   }
+
   render() {
+    const { from, to } = this.state.days
     return (
       <div>
         <UserForm />
@@ -17,9 +31,32 @@ export default class App extends Component {
           onChange={this.handleSelectChange}
           value={this.state.selectedOption}
         />
-        <ArticleList items={articles} />
+        {JSON.stringify(this.state)}
+        <br />
+        <DayPicker
+          onDayClick={this.onDayClickHandler}
+          selectedDays={[{ from, to }]}
+        />
+        <ArticleList items={this.filteredArticles} />
       </div>
     )
+  }
+
+  get filteredArticles() {
+    const result = articles.filter((article) => this.filterDays(article.date))
+    console.log(result)
+    return result
+  }
+  onDayClickHandler = (day) => {
+    const range = DateUtils.addDayToRange(day, this.state.days)
+    this.setState({ days: range })
+  }
+
+  filterDays = (day) => {
+    console.log(day)
+    const { days: range = { from: undefined, to: undefined } } = this.state
+    console.log(range)
+    return DateUtils.isDayInRange(new Date(day), range)
   }
 
   get optionsForSelect() {
